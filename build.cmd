143e1e45dae158bfd43e2fa5ca6322c2abce1288
jdevillard <jeremie.devillard@moskitos.fr>
2016-10-04 10:27:27 +0200

    add build command

---
+++ build.cmd
@@ -0,0 +1,8 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /m
+
+packages\NuGet.CommandLine.3.4.3\tools\nuget.exe pack -OutputDirectory artifacts\Release
